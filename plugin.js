6eb961a969cab849f300625e0ed9805632d1ff9f
Joe Skeen <joe.skeen@healthcatalyst.com>
2022-05-16 15:28:38 -0600

    add comment and license

--- plugin.js
+++ plugin.js
@@ -1,8 +1,14 @@
+/*
+ * Monaco Editor plugin for Reveal.js
+ * Version 2.0
+ * by Joe Skeen
+ * License: MIT
+ */
+
 let defaultOptions = {
   monacoBaseUrl: "https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0",
   selector: "pre code.monaco",
   defaultLanguage: "javascript",
-  minHeight: "350px",
   debug: false,
 };
 
