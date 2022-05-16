8717ff11dc42828e1432c6d117a5233eadd2eb0c
Joe Skeen <joe.skeen@healthcatalyst.com>
2022-05-16 15:11:47 -0600

    updated to work with latest Monaco and RevealJS

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# reveal-monaco
-A plugin for RevealJS to allow embedding the Monaco editor into presentations
+# Monaco Editor Plugin for Reveal.js
+
+A small plugin that allows you to use the Monaco Editor in your reveal.js presentations (i.e. live coding, etc.)
+
+## Setup
+
+* Copy the plugin file (`plugin.js`) into the `/plugin/monaco` folder of your presentation
+* Import and register the plugin: 
+    ```html
+    <script type="module">
+      import RevealMonaco from "./plugin/monaco/plugin.js";
+      // More info about initialization & config:
+      // - https://revealjs.com/initialization/
+      // - https://revealjs.com/config/
+      Reveal.initialize({
+        hash: true,
+
+        monaco: {
+          defaultLanguage: 'python' // optional, defaults to 'javascript'
+        },
+
+        // Learn about plugins: https://revealjs.com/plugins/
+        plugins: [
+          RevealMarkdown,
+          RevealNotes,
+          RevealMonaco,
+          // RevealHighlight <-- comment this one out!
+        ],
+      });
+    </script>
+    ```
+
+    Note that the built-in `RevealHighlight` plugin **will not** work with the Monaco plugin out-of-the-box.
+    Save yourself some headache and remove it (it's in the code snippet above by default).
+* To use the Monaco Editor in your slide, just add a preformatted code block with the class "monaco":
+    ```html
+    <section>
+      <h2>My Code</h2>
+      <!-- specifying the language below is optional, will fall back to default -->
+      <pre><code class="monaco" language="javascript">
+function helloWorld() {
+  console.log('hello, world!')
+}
+      </code></pre>
+    </section>
+    ```
+
+## Requirements
+
+Requires a browser capable of loading ECMAScript modules (no IE support)
---
+++ plugin.js
@@ -0,0 +1,115 @@
+let defaultOptions = {
+  monacoBaseUrl: "https://cdn.jsdelivr.net/npm/monaco-editor@0.33.0",
+  selector: "pre code.monaco",
+  defaultLanguage: "javascript",
+  debug: false,
+};
+
+export class MonacoPlugin {
+  constructor(reveal) {
+    this.deck = reveal;
+    this.activeEditor = null;
+    let revealOptions = this.deck.getConfig().monaco || {};
+    this.options = { ...defaultOptions, ...revealOptions };
+    this.monacoBaseUrl = this.options.monacoBaseUrl;
+  }
+
+  async init() {
+    await new Promise((resolve) =>
+      this.loadScript(`${this.monacoBaseUrl}/min/vs/loader.js`, resolve)
+    );
+    require.config({ paths: { vs: `${this.monacoBaseUrl}/min/vs` } });
+    await new Promise((resolve) => require(["vs/editor/editor.main"], resolve));
+    if (window.monaco) {
+      this.log("[monaco] loaded");
+      this.monaco = window.monaco;
+    } else {
+      throw new Error("Could not load Monaco");
+    }
+
+    this.deck.on("slidechanged", (x) => this.onSlideChanged(x));
+
+    // see if there is an editor on the initial slide
+    this.onSlideChanged();
+  }
+
+  loadScript(url, callback) {
+    let head = document.querySelector("head");
+    let script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = url;
+
+    // Wrapper for callback to make sure it only fires once
+    let finish = () => {
+      if (typeof callback === "function") {
+        callback.call();
+        callback = null;
+      }
+    };
+
+    script.onload = finish;
+
+    // IE
+    script.onreadystatechange = () => {
+      if (this.readyState === "loaded") {
+        finish();
+      }
+    };
+
+    // Normal browsers
+    head.appendChild(script);
+  }
+
+  onSlideChanged(event) {
+    const state = {
+      previousSlide: event?.previousSlide || this.deck.getPreviousSlide(),
+      currentSlide: event?.currentSlide || this.deck.getCurrentSlide(),
+    };
+    this.log(state);
+    if (state.previousSlide) {
+      const codeBlock = state.previousSlide.querySelector(
+        this.options.selector
+      );
+      if (codeBlock) {
+        if (this.activeEditor) {
+          const contents = this.activeEditor.getModel().getValue();
+          codeBlock.innerText = contents;
+          this.activeEditor.dispose();
+          this.activeEditor = null;
+        }
+      }
+    }
+    if (state.currentSlide) {
+      const codeBlock = state.currentSlide.querySelector(this.options.selector);
+      if (codeBlock) {
+        const initialCode = codeBlock.innerText;
+        codeBlock.innerHTML = "";
+        const language =
+          codeBlock.getAttribute("language") || this.options.defaultLanguage;
+        this.activeEditor = this.monaco.editor.create(codeBlock, {
+          value: initialCode,
+          language: language,
+        });
+      }
+    }
+  }
+
+  log(content) {
+    if (this.options.debug) {
+      console.info(content);
+    }
+  }
+}
+
+export const Plugin = () => {
+  return {
+    id: "monaco",
+
+    init: function (reveal) {
+      const plugin = new MonacoPlugin(reveal);
+      return plugin.init();
+    },
+  };
+};
+
+export default Plugin;
